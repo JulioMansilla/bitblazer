02e5dbf257767e5675d07ff40940b438224c0db5
Julio Mansilla <JulioMansilla@users.noreply.github.com>
2022-12-17 23:19:13 -0500

    Update hack.js
    Add host name logic.

--- scripts/hack.js
+++ scripts/hack.js
@@ -1,17 +1,19 @@
 /** @param {NS} ns */
 export async function main(ns) {
+	var host;
+	if (ns.args[0]) {
+		host = ns.args[0];
+	}
+	else {
+		host = ns.getHostname();
+	}
 
-	var host = ns.getHostname()
-	var cashTarget = ns.getServerMaxMoney(host) * 0.75
-	var securityTarget = ns.getServerSecurityLevel(host) + 5
+	var cashTarget = ns.getServerMaxMoney(host) * 0.75;
+	var securityTarget = ns.getServerSecurityLevel(host) + 5;
 
 	while (true) {
-		if (ns.getServerSecurityLevel(host) > securityTarget) {
-			await ns.weaken(host);
-		} else if (ns.getServerMoneyAvailable(host) < cashTarget) {
-			await ns.grow(host);
-		} else {
-			await ns.hack(host);
-		}
+		if (ns.getServerSecurityLevel(host) > securityTarget) { await ns.weaken(host); }
+		else if (ns.getServerMoneyAvailable(host) < cashTarget) { await ns.grow(host); }
+		else { await ns.hack(host); }
 	}
 }
